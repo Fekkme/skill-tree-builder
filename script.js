55d75f8afb32c2f9631166b795d59044d485d9f3
Fekkme <Fekkme@users.noreply.github.com>
2024-05-07 16:09:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 // Define a function to handle click event
-function handleClick(event) {
+function handleClick(event, const divElement) {
     console.log('Div clicked');
     // You can add your logic here for what happens when the div is clicked
-    var div = document.getElementById("skill-1");
+    var div = document.getElementById(divElement);
     var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     div.style.backgroundColor = randomColor;
 }
 
-// Get the div element by its ID
-const divElement = document.getElementById('skill-1');
-const divElement2 = document.getElementById('skill-2');
-
 alert("change1")
-// Check if the div element exists
-if (divElement) {
-    // Add event listener for click event
-    divElement.addEventListener('click', handleClick);
-} else {
-    console.error('Div element not found!');
-}
+
+// Get the div element by its ID
+const divElement1 = document.getElementById('skill-1');
+const divElement2 = document.getElementById('skill-2');
+const divElement3 = document.getElementById('skill-3');
+
+divElement1.addEventListener('click', handleClick(divElement1));
+divElement2.addEventListener('click', handleClick(divElement2));
+divElement3.addEventListener('click', handleClick(divElement3));
+
