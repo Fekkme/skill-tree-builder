18a10aba3981386b22f30e1ff2cbe4d4d986756a
John <jcarsing@gmail.com>
2024-05-08 09:38:40 +0800

    fix

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 // Define a function to handle click event
-function handleClick(event, divElement) {
-    console.log('Div clicked');
-    // You can add your logic here for what happens when the div is clicked
-    var div = document.getElementById(divElement);
-    var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-    div.style.backgroundColor = randomColor;
-}
+function handleClick(event) {
 
-alert("change1")
+    var clickedDivID = event.target.id;
+    var newParentDivID = getNewParentDivID(clickedDivID);
+
+    // Show below div
+    var BellowParentDiv = document.getElementById(newParentDivID);
+    var showDiv = BellowParentDiv.getElementsByClassName("flex-item").namedItem(clickedDivID)
+    showDiv.style = "show"
+
+}
 
 // Get the div element by its ID
 const divElement1 = document.getElementById('skill-1');
 const divElement2 = document.getElementById('skill-2');
 const divElement3 = document.getElementById('skill-3');
 
-divElement1.addEventListener('click', handleClick(divElement1));
-divElement2.addEventListener('click', handleClick(divElement2));
-divElement3.addEventListener('click', handleClick(divElement3));
+divElement1.addEventListener('click', handleClick);
+divElement2.addEventListener('click', handleClick);
+divElement3.addEventListener('click', handleClick);
 
+
+function getNewParentDivID(childDivID){
+
+    var div = document.getElementById(childDivID);
+    var newParentDivId = div.parentNode;
+
+    var levelNr = parseInt(newParentDivId.id.substring(6));
+    levelNr++
+
+    return 'level-' + levelNr
+
+}
